4fd92420453b43728952aa65b178386b4853a54f
visualstudio <visualstudio@122-PC28>
2019-04-12 19:27:43 -0400

    Add project files.

---
+++ c0725563-assign5-section2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0725563_assign5_section2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
